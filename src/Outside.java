1c0a3a75a73f8dbd66a30c7a21ccba7e9b4d4875
pravinbhagwat <pravinbhagwat150@gmail.com>
2022-12-14 14:10:19 +0530

    Dev Second

--- src/Outside.java
+++ src/Outside.java
@@ -1,4 +1,8 @@
 public class Outside {
+
+    public void display() {
+        System.out.println("Disply");
+    }
     public static void main(String[] args) {
         System.out.println("Hello");
     }
